ea7c8ccbe46348e654ca5cec046bb5ab4b0e1799
BardechoO0 <bardecho00@gmail.com>
2024-11-19 17:17:13 +0100

    dsf

--- Assets/Scripts/Samba_2/Parte_2/Random.cs
+++ Assets/Scripts/Samba_2/Parte_2/Random.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class Random : MonoBehaviour
 {
+    [Header("Intaciar")]
+
     [SerializeField] GameObject[] asset = new GameObject[2];
+
+    [SerializeField] BoxCollider gameplayArea;
 
     [SerializeField] Transform Pos;
 
@@ -19,7 +23,7 @@ public class Random : MonoBehaviour
 
             print(random);
 
-            if (random == 0)
+            /*if (random == 0)
             {
                 Instantiate(asset[random], iniPos, Quaternion.Euler(0, 0, 0));
             }
@@ -27,11 +31,27 @@ public class Random : MonoBehaviour
             {
                 Instantiate(asset[random], iniPos + new Vector3(-10,0,0), Quaternion.Euler(0, 0, 0));
                 
-            }
+            }*/
 
             iniPos += new Vector3(0, 0, UnityEngine.Random.Range(5, 10));
 
+
+            //La otra opcion
+
+            Vector3 cambioPos = gameplayArea.bounds.size/2;
+
+            Vector3 ramdomPos = new Vector3(
+                UnityEngine.Random.Range(-cambioPos.x, cambioPos.x),
+                UnityEngine.Random.Range(-cambioPos.y, cambioPos.y),
+                UnityEngine.Random.Range(-cambioPos.z, cambioPos.z)
+               );
+
+            Instantiate(asset[random], ramdomPos +  gameplayArea.transform.position, Quaternion.identity);
+
         }
+
+
+
     }
 
     // Update is called once per frame
